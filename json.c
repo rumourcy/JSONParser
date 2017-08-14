5db6d09678796c71a4c5359e732471aedfc03120
trierbo <trier@keessi.com>
2017-08-14 21:19:32 +0800

    print

--- json.c
+++ json.c
@@ -3,9 +3,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <float.h>
+#include <limits.h>
 
 // 因为数据格式错误，导致解析错误，退出指针所指的位置
 static const char *ep;
+
+typedef struct {
+  char *buffer;
+  int length;
+  int offset;
+} printbuffer;
 
 // unicode to UTF-8
 static const unsigned char firstByteMark[7] = {0x00, 0x00, 0xC0, 0xE0, 0xF0, 0xF8, 0xFC};
@@ -21,6 +29,7 @@ static void (*JSON_free)(void *ptr) = free;
 static const char *parse_value(JSON *item, const char *value);
 static const char *parse_array(JSON *item, const char *value);
 static const char *parse_object(JSON *item, const char *value);
+static char *print_value(JSON *item,int depth,int fmt,printbuffer *p);
 
 static JSON *JSON_New_Item(void) {
   JSON *node = (JSON*)JSON_malloc(sizeof(JSON));
@@ -34,6 +43,49 @@ static const char *skip(const char *in) {
   while (in && *in && (unsigned char)*in <= 32)
     in++;
   return in;
+}
+
+static int update(printbuffer *p) {
+  char *str;
+  if (!p || !p->buffer)
+    return 0;
+  str = p->buffer + p->offset;
+  return p->offset + strlen(str);
+}
+
+static int pow2gt(int x) {
+  --x;
+  x |= x >> 1;
+  x |= x >> 2;
+  x |= x >> 4;
+  x |= x >> 8;
+  x |= x >> 16;
+  return x + 1;
+}
+
+static char *ensure(printbuffer *p, int needed) {
+  char *newbuffer;
+  int newsize;
+  if (!p || !p->buffer)
+    return 0;
+  needed += p->offset;
+  if (needed <= p->length)
+    return p->buffer + p->offset;
+
+  newsize = pow2gt(needed);
+  newbuffer = (char*)JSON_malloc(newsize);
+  if (!newbuffer) {
+    JSON_free(p->buffer);
+    p->length = 0;
+    p->buffer = 0;
+    return 0;
+  }
+  if (newbuffer)
+    memcpy(newbuffer, p->buffer, p->length);
+  JSON_free(p->buffer);
+  p->length = newsize;
+  p->buffer = newbuffer;
+  return newbuffer + p->offset;
 }
 
 // 16进制转10进制
@@ -135,6 +187,79 @@ static const char *parse_string(JSON *item, const char *str) {
   return ptr;
 }
 
+// 打印字符串
+static char *print_string_ptr(const char *str, printbuffer *p) {
+  const char *ptr;
+  char *ptr2, *out;
+  int len = 0, flag = 0;
+  unsigned char token;
+
+  for (ptr=str; *ptr; ptr++)
+    flag |= ((*ptr > 0 && *ptr < 32) || (*ptr == '\"') || (*ptr == '\\')) ? 1 : 0;
+  if (!flag) {
+    len = ptr - str;
+    // '\"' '\"' '\0'
+    if (p) out = ensure(p, len + 3);
+    else out = (char*)JSON_malloc(len + 3);
+    if (!out) return 0;
+    ptr2 = out;
+    *ptr2++ = '\"';
+    strcpy(ptr2, str);
+    ptr2[len] = '\"';
+    ptr2[len+1] = 0;
+    return out;
+  }
+
+  // 字符串为空
+  if (!str) {
+    if (p) out = ensure(p, 3);
+    else out = (char*)JSON_malloc(3);
+    if (!out) return 0;
+    strcpy(out, "\"\"");
+    return out;
+  }
+  
+  ptr = str;
+  while ((token = *ptr) && ++len) {
+    if (strchr("\"\\\b\f\n\r\t", token))
+      len++;
+    else if (token < 32) //'\u'
+      len += 5;
+    ptr++;
+  }
+  if (p) out = ensure(p, len + 3);
+  else out = (char*)JSON_malloc(len + 3);
+  if (!out) return 0;
+
+  ptr2 = out;
+  ptr = str;
+  *ptr2++ = '\"';
+  while (*ptr) {
+    if ((unsigned char)*ptr > 31 && *ptr != '\"' && *ptr != '\\')
+      *ptr2++ = *ptr++;
+    else {
+      *ptr2++ = '\\';
+      switch (token = *ptr++) {
+      case '\\': *ptr2++ = '\\'; break;
+      case '\"': *ptr2++ = '\"'; break;
+      case '\b': *ptr2++ = 'b'; break;
+      case '\f': *ptr2++ = 'f'; break;
+      case '\n': *ptr2++ = 'n'; break;
+      case '\r': *ptr2++ = 'r'; break;
+      case '\t': *ptr2++ = 't'; break;
+      default: sprintf(ptr2, "u%04x", token); ptr2 += 5; break;
+      }
+    }
+  }
+  *ptr2++ = '\"';
+  *ptr2++ = 0;
+  return out;
+}
+
+static char *print_string(JSON *item, printbuffer *p) {
+  return print_string_ptr(item->valuestring, p);
+}
+
 // 解析数字
 static const char *parse_number(JSON *item, const char *num) {
   double n = 0, sign = 1, scale = 0;
@@ -169,6 +294,33 @@ static const char *parse_number(JSON *item, const char *num) {
   return num;
 }
 
+// 打印数字
+static char *print_number(JSON *item, printbuffer *p) {
+  char *str = 0;
+  double d = item->valuedouble;
+  if (d == 0) {
+    if (p) str = ensure(p, 2);
+    else str = (char*)JSON_malloc(2);
+    if (str) strcpy(str, "0");
+  } else if (fabs(((double)item->valueint)-d) <= DBL_EPSILON && d <= INT_MAX && d >= INT_MIN) {
+    if (p) str = ensure(p, 21);
+    else str = (char*)JSON_malloc(21);
+    if (str) sprintf(str, "%d", item->valueint);
+  } else {
+    if (p) str = ensure(p, 64);
+    else str = (char*)JSON_malloc(64);
+    if (str) {
+      if (fabs(floor(d)-d) <= DBL_EPSILON && fabs(d) < 1.0e60)
+        sprintf(str, "%.0f", d);
+      else if (fabs(d) < 1.0e-6 || fabs(d) > 1.0e9)
+        sprintf(str, "%e", d);
+      else
+        sprintf(str, "%f", d);
+    }
+  }
+  return str;
+}
+
 // 解析JSON数组
 static const char *parse_array(JSON *item, const char *value) {
   JSON *child;
@@ -200,6 +352,97 @@ static const char *parse_array(JSON *item, const char *value) {
   if (*value == ']') return value + 1;
   ep = value;
   return 0;
+}
+
+
+// 打印JSON数组
+static char *print_array(JSON *item, int depth, int fmt, printbuffer *p) {
+  char **entries;
+  char *out = 0, *ptr, *ret;
+  int len = 5;
+  JSON *child = item->child;
+  int numentries = 0, i = 0, fail = 0;
+  size_t tmplen = 0;
+
+  while (child) numentries++, child = child->next;
+  if (!numentries) {
+    if (p) out = ensure(p, 3);
+    else out = (char*)JSON_malloc(3);
+    if (out) strcpy(out, "[]");
+    return out;
+  }
+
+  if (p) {
+    i = p->offset;
+    ptr = ensure(p, 1);
+    if (!ptr) return 0;
+    *ptr = '[';
+    p->offset++;
+    child = item->child;
+    while (child && !fail) {
+      print_value(child, depth+1, fmt, p);
+      // 更新偏移位
+      p->offset = update(p);
+      if (child->next) {
+        len = fmt ? 2 : 1;
+        ptr = ensure(p, len+1);
+        if (!ptr)
+          return 0;
+        *ptr++ = ',';
+        if (fmt)
+          *ptr++ = ' ';
+        *ptr = 0;
+        p->offset+=len;
+      }
+      child = child->next;
+    }
+    ptr = ensure(p, 2);
+    if (!ptr) return 0;
+    *ptr++ = ']';
+    *ptr = 0;
+    out = (p->buffer) + i;
+  } else {
+    entries = (char**)JSON_malloc(numentries*sizeof(char*));
+    if (!entries) return 0;
+    memset(entries, 0, numentries*sizeof(char*));
+    child=item->child;
+    while(child && !fail) {
+      ret = print_value(child, depth+1, fmt, 0);
+      entries[i++] = ret;
+      if (ret) len += strlen(ret) + 2 + (fmt ? 1 : 0);
+      else fail = 1;
+      child = child->next;
+    }
+
+    if (!fail) out = (char*)JSON_malloc(len);
+    if (!out) fail = 1;
+    if (fail) {
+      for (i = 0; i < numentries; i++)
+        if (entries[i])
+          JSON_free(entries[i]);
+      JSON_free(entries);
+      return 0;
+    }
+    *out = '[';
+    ptr = out + 1;
+    *ptr = 0;
+    for (i = 0; i < numentries; i++) {
+      tmplen = strlen(entries[i]);
+      memcpy(ptr, entries[i], tmplen);
+      ptr += tmplen;
+      if (i != numentries - 1) {
+        *ptr++ = ',';
+        if (fmt)
+          *ptr++ = ' ';
+        *ptr = 0;
+      }
+      JSON_free(entries[i]);
+    }
+    JSON_free(entries);
+    *ptr++ = ']';
+    *ptr++ = 0;
+  }
+  return out;
 }
 
 //解析JSON对象
@@ -280,6 +523,10 @@ static const char *parse_value(JSON *item, const char *value) {
   }
   ep = value;
   return 0;
+}
+
+static char *print_value(cJSON *item,int depth,int fmt,printbuffer *p) {
+  
 }
 
 // TODO
